1f465e499e122f9faa4f7c0442a9de8475ffedb8
fu4dys <fuadysyukur01@gmail.com>
2021-10-24 21:56:38 +0700

    Jumping system added

--- endless running/Assets/PlayerController.cs
+++ endless running/Assets/PlayerController.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float gravity;
+    public Vector2 velocity;
+    public float jumpVelocity = 20f;
+    public float groundHeight = 10f;
+    public bool isGrounded = false;
+
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Vector2 pos = transform.position;
+
+        if(!isGrounded)
+        {
+            pos.y += velocity.y * Time.fixedDeltaTime;
+            velocity.y += gravity * Time.fixedDeltaTime;
+
+            if(pos.y <= groundHeight)
+            {
+                pos.y = groundHeight;
+                isGrounded = true;
+                Debug.Log("Grounded");
+            }
+        }
+        transform.position = pos;
+    }
+
+    private void Update()
+    {
+        if(isGrounded)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                isGrounded = false;
+                velocity.y = jumpVelocity;
+            }
+        }
     }
 }
